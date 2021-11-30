523dae7ce67b5acf39c4c67a0c62e94179658599
Peverall <code.statera@gmail.com>
2021-11-30 04:34:49 -0800

    first easy entry on google app engine commit

---
+++ easyentry-on-google-app-engine/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>This Easy Entry app will be deployed on Google App Engine via Github</h1>
+    </div>
+  );
+}
+
+export default App;
